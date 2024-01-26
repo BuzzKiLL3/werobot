524d457e3f5a0e0d765d87a35f7821826aa3dd07
BuzzKiLL3 <155957176+BuzzKiLL3@users.noreply.github.com>
2024-01-26 10:22:44 +0100

    Add files via upload

---
+++ Demo1.py
@@ -0,0 +1,85 @@
+from pyniryo2 import *
+
+# - Constants
+workspace_name = "workspace_fixe_V2"  # Robot's Workspace Name
+robot_ip_address = "10.89.5.15"
+
+# The pose from where the image processing happens
+observation_pose = PoseObject(
+    x=0.024, y=0.2, z=0.25,
+    roll=-2.939, pitch=1.359, yaw=-1.352,
+)
+
+#Intermediate place
+intermediate_place = PoseObject(
+    x=0.14, y=0, z=0.2,
+    roll=1.49, pitch=1.54, yaw=1.57
+)
+
+#center mobile robot position
+mobile_robot_point = [-0.08, -1.44,-0.29, -0.18, 0.15, 0.02]
+
+# Place pose square
+place_pose_square_red = PoseObject(
+    x=0.198, y=0.033, z=-0.0992,
+    roll=-2.853, pitch=1.473, yaw=-2.799
+)
+
+# Place pose circle
+place_pose_circle_red=place_pose_square_red.copy_with_offsets(z_offset=0.02)
+
+# Place pose square bleu
+place_pose_square_bleu=place_pose_square_red.copy_with_offsets(x_offset=0.05)
+
+# Place pose square bleu
+place_pose_circle_bleu=place_pose_square_bleu.copy_with_offsets(z_offset=0.02)
+# Place pose square bleu
+place_pose_square_green=place_pose_square_red.copy_with_offsets(y_offset=0.05)
+# Place pose square bleu
+place_pose_circle_green=place_pose_square_green.copy_with_offsets(z_offset=0.02)
+# - Initialization
+
+# Connect to robot
+robot = NiryoRobot(robot_ip_address)
+# Calibrate robot if robot needs calibration
+robot.arm.calibrate_auto()
+# Updating tool
+robot.tool.update_tool()
+
+###############################3
+
+
+max_failure_count = 3
+
+
+#Functions
+
+
+def get_object(object_shape, object_color,place):
+    try_without_success = 0
+    while try_without_success < max_failure_count:
+    # Moving to observation pose
+        robot.arm.move_pose(observation_pose)
+    # Trying to get object via Vision Pick
+        obj_found, shape, color = robot.vision.vision_pick(workspace_name, shape = object_shape, color = object_color)
+        if not obj_found:
+            try_without_success += 1
+            robot.wait(0.1)
+            continue  
+
+        robot.arm.move_pose(intermediate_place)
+        robot.arm.move_pose(place)
+        robot.tool.grasp_with_tool()
+        robot.tool.open_gripper(speed=500)
+        robot.arm.move_pose(intermediate_place)
+
+get_object(ObjectShape.SQUARE,ObjectColor.RED,place_pose_square_red)
+get_object(ObjectShape.SQUARE, ObjectColor.GREEN,place_pose_square_green)
+get_object(ObjectShape.SQUARE, ObjectColor.BLUE,place_pose_square_bleu)
+get_object(ObjectShape.CIRCLE,ObjectColor.BLUE,place_pose_circle_bleu)
+get_object(ObjectShape.CIRCLE, ObjectColor.GREEN,place_pose_circle_green)
+get_object(ObjectShape.CIRCLE, ObjectColor.RED,place_pose_circle_red)
+
+robot.arm.go_to_sleep()
+
+robot.end()
